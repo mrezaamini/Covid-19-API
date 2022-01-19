af3195b43355beff3e294b68dbd9ddf593655faa
Mohammadreza Amini <aminiamini433@yahoo.fr>
2022-01-19 21:41:00 +0330

    public user CRUD

--- Routes/public.js
+++ Routes/public.js
@@ -1,26 +1,43 @@
 //What Public Users Can Do
 const router = require('express').Router()
+const { query } = require('express');
 const CountrySchema = require('../models/country')
-const {newCountry_validation} = require('../validation')
+const {newCountry_validation, sortQuery_validation} = require('../validation')
+
 
 router.get('/:countryName',async(req, res)=>{
-   // res.send(`get country info for ${req.params.countryName}`)
     const {error} = newCountry_validation(req.params)
     if(error){
         return res.status(400).send(error.details[0].message)
     }
     let country = await CountrySchema.findOne({ country: req.params.countryName.toLowerCase() });
     if (!country){
-        return res.status(404).send('Country Does Not Exist!');
+        return res.status(404).send('Country Does Not Exist!'); // country not found
     }
-    res.status(200).send(country)
+    res.status(200).send(country) //country found 
 })
-router.get('/',(req, res)=>{
-    if(req.query.sort){
-        res.send(`all sort by ${req.query.sort}`)
+router.get('/',async(req, res)=>{
+    if(req.query.sort){ //check query for sort
+       const {error} = sortQuery_validation(req.query) //validating sort query to be in valid cases
+       if(error){
+           return res.status(400).send(error.details[0].message)
+       }
+       const searchBase = ("{"+JSON.stringify(req.query.sort)+":"+(-1)+"}") //make e sort basis and set it to desc
+       const tojson = JSON.parse(searchBase)
+       try{
+           const allCountries = await CountrySchema.find().sort(tojson) //get sorted countries
+           res.status(200).json(allCountries)
+       } catch(err){
+           res.status(500).json(err) //server error
+       }
     }
     else{
-        res.send('all country data')
+        try{
+            const allCountries = await CountrySchema.find()
+            res.status(200).json(allCountries)
+        } catch (err){
+            res.status(500).json(err) //server error
+        }
     }
 })
 
