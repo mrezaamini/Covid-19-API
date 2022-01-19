ed15fa82a7dc429c9b3cad5d99ae8352e2158b16
Mohammadreza Amini <aminiamini433@yahoo.fr>
2022-01-19 20:27:07 +0330

    super admin route update

--- Routes/superAdmin.js
+++ Routes/superAdmin.js
@@ -1,17 +1,18 @@
 //What SuperAdmins Do
 const router = require('express').Router()
 const UserSchema = require('../models/user')
-const {adminUserPass_validation} = require('../validation')
+const CountrySchema = require('../models/country')
+const {adminUserPass_validation, newCountry_validation} = require('../validation')
 
-router.delete('/admin', async(req,res)=>{
+router.delete('/admin', async(req,res)=>{ //to check db
     const deleted = await UserSchema.deleteMany();
 })
 
-router.get('/admin', async(req, res)=>{
+router.get('/admin', async(req, res)=>{ // to check db
     const admins = await UserSchema.find();
     res.json(admins)
 })
-
+//Make new admin with username and password
 router.post('/admin', async (req, res)=>{
     //validate new admin username and password
     const {error} = adminUserPass_validation(req.body)
@@ -35,9 +36,37 @@ router.post('/admin', async (req, res)=>{
     }
 })
 
-router.post('/countries/:countryName', (req, res)=>{
-    res.send(`make new empty country ${req.params.countryName}`)
+// Make new country with initial value of all cases equal to 0
+router.post('/countries/:countryName', async(req, res)=>{ 
+    //validate new country
+    const {error} = newCountry_validation(req.params)
+    if(error){
+        return res.status(400).send(error.details[0].message)
+    }
+    let country = await CountrySchema.findOne({ country: req.params.countryName.toLowerCase() });
+    if (country){
+        return res.status(400).send('Country already exists!');
+    } 
+
+    const newCountry = new CountrySchema({
+        country:req.params.countryName.toLowerCase()
+    })
+    try{
+        const savedCountry = await newCountry.save() 
+        res.send(savedCountry)
+    }catch(err){
+        res.status(400).send(err)
+    }
 })
+
+router.get('/allCountry', async(req, res)=>{ // to check db
+    const countries = await CountrySchema.find();
+    res.json(countries)
+})
+router.delete('/allCountry', async(req,res)=>{ //to check db
+    const deleted = await CountrySchema.deleteMany();
+})
+
 
 router.put('/countries/:countryName',(req, res)=>{
     res.send(`change permissions ${req.params.countryName}`)
---
+++ models/country.js
@@ -0,0 +1,38 @@
+//country model
+const mongoose = require('mongoose')
+
+const countrySchema = new mongoose.Schema({
+    country:{
+        type:String,
+        required:true,
+        min:3,
+        max:30
+    },
+    todayCases:{
+        type:Number,
+        min:0,
+        required:true,
+        default:0
+
+    },
+    todayDeaths:{
+        type:Number,
+        min:0,
+        required:true,
+        default:0
+    },
+    todayRecovered:{
+        type:Number,
+        min:0,
+        required:true,
+        default:0
+    },
+    critical:{
+        type:Number,
+        min:0,
+        required:true,
+        default:0
+    }
+})
+
+module.exports = mongoose.model('CountrySchema', countrySchema)
