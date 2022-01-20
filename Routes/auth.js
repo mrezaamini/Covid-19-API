2a087622fe5f6dd2197cc92a23f85d9638d5ec7b
Mohammadreza Amini <aminiamini433@yahoo.fr>
2022-01-20 17:35:55 +0330

    auth done

--- Routes/auth.js
+++ Routes/auth.js
@@ -7,21 +7,18 @@ const {adminUserPass_validation} = require('../validation')
 const {ROLES} = require('../config/roles_list')
 
 
-router.post('/login',async(req, res)=>{ 
-  //TODO: validating login
+router.post('/login',async(req, res)=>{ //loging user in and giving it a specific token based on user name and id
   const user = await UserSchema.findOne({username : req.body.adminUsername})
   if(!user) return res.status(404).send('Username is not found')
   const valid_pass = await bcrypt.compare(req.body.adminPassword, user.password)
   if(!valid_pass) return res.status(400).send('Password is incorrect')
-
-  //create and assign token 
   const token = JWT.sign({_id: user._id, adminUsername: user.adminUsername, role:user.role},
     '86a0f66f177fd7d8dd254e9f57f2057e70f9c3aa8c4719460eeb30056304910c2095819c0fdd796b4caef488c54b87b74e64592dd9c4973dafa368f27d33d8b5')
   res.header('user-token', token).send(token)
 })
 
 
-router.post('/register/superadmin', async(req,res)=>{
+router.post('/register/superadmin', async(req,res)=>{ //registering a super admin
   const {error} = adminUserPass_validation(req.body)
     if(error){
         return res.status(400).send(error.details[0].message)
